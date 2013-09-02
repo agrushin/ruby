aa20b7f5c3b0c13264075740f3bf58e22e5f7476
Alexander Grushin <a.grushin@gmail.com>
2013-09-02 17:01:25 +0400

    Use RabbitMQ API call to get queues info

--- check_amqp.rb
+++ check_amqp.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
+#
 require 'yaml'
-
-testarr={}
-testarr['simple.normal.queue.name1'] = 100
-testarr['simple.warning.queue.name'] = 10000
-testarr['rexp.big.queue.name.bulk'] = 100000
-testarr['sbulk1.huge.queue.name'] = 100000000
+require 'optparse'
+require 'net/http'
+require 'uri'
+require 'rubygems'
+require 'json'
 
 $config = YAML.load_file("check_amqp.yml")
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: check_amqp.rb [options]"
+  opts.on('-H','--hostname FQDN','RabbitMQ server') { |v| options[:hostname] = v }
+end.parse!
+
+raise OptionParser::MissingArgument if options[:hostname].nil?
 
 $status_code = {}
 $status_code["OK"] = 0
@@ -19,7 +27,11 @@ $state_code = $status_code["OK"]
 $state_msg = ""
 
 def raiseState( newstate, msg )
-  if newstate == :critical    # -> CRITICAL
+
+  if newstate == :unknown
+    $state_code = $status_code["UNKNOWN"]
+    $state_msg << msg
+  elsif newstate == :critical    # -> CRITICAL
     $state_code = $status_code["CRITICAL"]
     $state_msg << msg
   elsif newstate == :warning && $state_code == $status_code["OK"]   # ok -> WARNING
@@ -28,6 +40,7 @@ def raiseState( newstate, msg )
   else
     $state_msg << msg
   end
+
 end
 
 def getQueueThresholds( queueName )
@@ -46,18 +59,39 @@ def getQueueThresholds( queueName )
   return _threshold
 end
 
-testarr.each do |queuename, size, type|
+# - RabbitMQ management API (http://hg.rabbitmq.com/rabbitmq-management/raw-file/rabbitmq_v3_1_5/priv/www/api/index.html)
+uri = URI("http://#{options[:hostname]}:55672/api/queues?columns=name,messages,messages_unacknowledged")
+http = Net::HTTP.new( uri.host, uri.port )
+req = Net::HTTP::Get.new( uri.request_uri )
+req.basic_auth( "guest", "guest" )
 
-  thresholds = getQueueThresholds(queuename)
+begin
+  response = http.request(req)
+  qlist = JSON.parse(response.body)
+rescue Timeout::Error, Errno::ECONNREFUSED, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+  raiseState(:unknown, "#{e}")
+end
 
-  if size > thresholds['general']['critical']
-    raiseState(:critical, "C:#{queuename}:#{size} ")
-  elsif size > thresholds['general']['warning']
-    raiseState(:warning, "W:#{queuename}:#{size} ")
+if $state_code == $status_code["OK"]
+  qlist.each do |q|
+    thresholds = getQueueThresholds(q['name'])
+
+    if q['messages'] > thresholds['general']['critical']
+      raiseState(:critical, "C:#{q['name']}:#{q['messages']} ")
+    elsif q['messages'] > thresholds['general']['warning']
+      raiseState(:warning, "W:#{q['name']}:#{q['messages']} ")
+    end
+
+    if thresholds['unacked']
+      if q['messages_unacknowledged'] > thresholds['unacked']['critical']
+        raiseState(:critical, "UC:#{q['name']}:#{q['messages_unacknowledged']} ")
+      elsif q['messages_unacknowledged'] > thresholds['unacked']['warning']
+        raiseState(:warning, "UW:#{q['name']}:#{q['messages_unacknowledged']} ")
+      end
+    end
+
   end
-
 end
 
 puts "AMQP queues is #{$status_code.index($state_code)}: #{$state_msg}"
 exit $state_code
-
